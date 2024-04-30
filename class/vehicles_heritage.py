8e2c536e7bf89a06697b5f45b227ca727bf4a3a0
Felipe França <felipefrancavieira5@gmail.com>
2024-04-30 01:06:53 -0300

    feat: created class vehicles

---
+++ class/vehicles_heritage.py
@@ -0,0 +1,14 @@
+
+### Developing heritage
+
+class vehicles :
+
+    def __init__(self,color,license_plate,n_wheels) -> None:
+        self.color = color
+        self.license_plate = license_plate
+        self.n_wheels = n_wheels
+
+    def start_engine(self) -> str:
+        print("starting engine...")
+        print("engine running!")
+    
