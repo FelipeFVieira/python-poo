6f66a7791b9a1f4826c85cd678d8979fc8be0bf8
Felipe França <felipefrancavieira5@gmail.com>
2024-04-30 01:20:24 -0300

    feat: new classes and instantiating other vehicles

--- class/vehicles_heritage.py
+++ class/vehicles_heritage.py
@@ -9,15 +9,37 @@ class vehicles :
         self.n_wheels = n_wheels
 
     def start_engine(self) -> str:
-        print("starting engine...")
+        print(f"{self.__class__.__name__} starting engine...")
         print("engine running!")
     
 class car(vehicles):
     pass
 
 
+class motorcycle(vehicles):
+    pass
+
+class truck(vehicles):
+    pass
+
+
 ### Demonstrating how inheritance works
+
+
+### Car
 
 car = car("red","abcd-123",4)
 print(car)
 car.start_engine()
+
+### Motorcycle
+
+motorcycle = motorcycle("lightblue","dcba-321",2)
+print(motorcycle)
+motorcycle.start_engine()
+
+### Truck
+
+truck = truck("red","abcd-321",8)
+print(truck)
+truck.start_engine()
