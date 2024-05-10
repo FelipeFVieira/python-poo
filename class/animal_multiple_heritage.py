1ea44194b7133550cc8a800218a2e3382fb69070
FelipeFVieira <devfelipefranca@gmail.com>
2024-05-10 20:13:26 -0300

    feat: creating the project scope

---
+++ class/animal_multiple_heritage.py
@@ -0,0 +1,26 @@
+class Animal():
+    pass
+
+
+class Mammal(Animal):
+    pass
+
+
+class Birds(Animal):
+    pass
+
+
+class Dog(Mammal):
+    pass
+
+
+class Cat(Mammal):
+    pass
+
+
+class Lion(Mammal):
+    pass
+
+
+class Platyplus(Mammal, Birds):
+    pass
