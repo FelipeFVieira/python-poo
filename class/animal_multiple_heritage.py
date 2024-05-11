907dc66d82cdb8fb165c262cdb70476c8b110c03
FelipeFVieira <devfelipefranca@gmail.com>
2024-05-10 22:10:22 -0300

    docs: commenting code

--- class/animal_multiple_heritage.py
+++ class/animal_multiple_heritage.py
@@ -1,3 +1,8 @@
+
+### Multiple heritage
+
+### Developing heritage 
+
 class Animal():
     def __init__(self, n_paws, n_eyes,  ) -> None:
         self.n_paws = n_paws
@@ -43,18 +48,38 @@ class Platyplus(Mammal, Birds):
     def grunt(self) -> None:
         print("i'm grunting")
 
+### Demonstrating how multiple inheritance works
+
+### dog is a simple heritage 
+
 dog = Dog(fur_color="caramel",n_paws=4,n_eyes=2)
 print(dog)
 dog.bark()
+
+### cat is a simple heritage 
 
 cat = Cat(fur_color="black",n_paws=4,n_eyes=2)
 print(cat)
 cat.meow()
 
+### lion is a simple heritage 
+
 lion = Lion(fur_color="orange",n_paws=4,n_eyes=2)
 print(lion)
 lion.roar()
 
+### platyplus is not a simple heritage 
+
+### why?
+
+### the platyplus is animal but is birds too, what now?
+
+### So he has properties of birds and animals, and in order not to have a conflict between the super constructors, 
+### We pass the properties (arguments) as keywords and then python understands that it must use what it needs from 
+### Each constructor to instantiate the object with multiple inheritance.
+
 platyplus = Platyplus(fur_color="brown",beak_color="orange",n_paws=4,n_eyes=2)
 print(platyplus)
 platyplus.grunt()
+
+### Here you go!!!
